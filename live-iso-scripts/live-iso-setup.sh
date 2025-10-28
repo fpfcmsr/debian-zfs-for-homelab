d117abd9e310c9621bbe4a34f4cf95a49a32c7db
fpfcmsr <fpf@andrew.cmu.edu>
2025-10-28 05:02:12 +0000

    Create live-iso-setup.sh

---
+++ live-iso-scripts/live-iso-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Identify your two SSDs via /dev/disk/by-id/*
+ls -l /dev/disk/by-id
+
+export DISK1="/dev/disk/by-id/PUT-YOUR-FIRST-DISK-ID"
+export DISK2="/dev/disk/by-id/PUT-YOUR-SECOND-DISK-ID"
+
+# Required secrets (non-interactive). Use strong secrets!
+export LUKS_PASSPHRASE='Correct-Horse-Battery-Staple'
+export USERNAME='coolname'
+export USER_PASSWORD='ChangeMe'
+
+# Optional customizations (defaults shown):
+export HOSTNAME='debian'
+export TIMEZONE='Etc/UTC'
+export LOCALE='en_US.UTF-8'
+export BOOT_POOL_SIZE='2G'
+export CREATE_SWAP='no'
+export SWAP_SIZE='4G'
+export REBOOT_WHEN_DONE='yes'
+
+# then run as root: bash ./install-zfs-root.sh
